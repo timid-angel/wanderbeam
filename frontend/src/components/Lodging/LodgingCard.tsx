11b1d54f6cff8b833e66ecac73dae00b2cdda562
ephrem <ephremmamo555@gmail.com>
2024-07-16 12:58:56 +0300

    created lodging card

---
+++ frontend/src/components/Lodging/LodgingCard.tsx
@@ -0,0 +1,43 @@
+import hotel from "../../assets/hotel.webp";
+import "./LodgingCard.css";
+import { StarRate } from "@mui/icons-material";
+const LodgingCard = () => {
+  return (
+    <div className="lodging-card">
+      <img src={hotel} alt="hotel" />
+      <div className="lodging-content">
+        <h3><span>Name</span>: Continental Hotel</h3>
+        <p className="description">
+          Lorem ipsum dolor sit amet consectetur. Ornare sagittis tortor duis
+          congue accumsan bibendum in. In placerat et nibh suscipit semper nam
+          consequat. Nulla at amet velit ac adipiscing euismod. Aliquet sed id
+          vestibulum amet.
+        </p>
+        <p className="catagory"><span>Budget Per Night</span> : 1000Birr</p>
+        <p className="catagory"><span>Catagory</span> : Hotel</p>
+        <p className="catagory"><span>Locaton</span> : Beach City</p>
+        <h3 className="rate">Ratings</h3>
+        <div className="ratings">
+          <div>
+            <p>Quality Rating:</p>
+            <div className="stars">
+              <StarRate />
+              <StarRate />
+              <StarRate />
+            </div>
+          </div>
+          <div>
+            <p>Users Rating: </p>{" "}
+            <div className="stars">
+              <StarRate />
+              <StarRate />
+            </div>
+          </div>
+        </div>
+        <button className="detail-button">View Detail</button>
+      </div>
+    </div>
+  );
+};
+
+export default LodgingCard;
