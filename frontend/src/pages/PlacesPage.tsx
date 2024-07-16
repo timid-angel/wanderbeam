7b630f946ab26878dd1085e5573f4500d3232617
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-16 23:18:18 +0300

    feat(pages): add places list to manage place list

--- frontend/src/pages/PlacesPage.tsx
+++ frontend/src/pages/PlacesPage.tsx
@@ -3,47 +3,49 @@ import { getPlaces, PlaceData } from "../API/places";
 import "../styles/output.css";
 import SearchBar from "../components/core/SearchBar";
 import Pagination from "../components/core/PaginationComponent";
-import PlacesCard from "../components/Places/PlacesCard";
 import Header from "../components/core/Header";
 import Footer from "../components/core/Footer";
+import PlacesList from "../components/Places/PlacesList";
 
 const PlacesPages = () => {
-  const [places, setPlaces] = useState<PlaceData[] | null>(null);
+  const [places, setPlaces] = useState<PlaceData[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
   useEffect(() => {
-    let fetchedPlaces = null;
-    try {
-      fetchedPlaces = getPlaces();
-    } catch {
-      console.log("error");
-    }
-    setPlaces(fetchedPlaces);
+    const fetchPlaces = async () => {
+      try {
+        const fetchedPlaces = await getPlaces();
+        setPlaces(fetchedPlaces);
+      } catch (error) {
+        console.log("Error fetching places:", error);
+      }
+    };
+    fetchPlaces();
   }, []);
 
-  function HandleSearch(searchTerm: string): void {
-    if (!searchTerm) {
-      throw new Error("Function not implemented.");
-    }
-  }
+  const handleSearch = (searchTerm: string) => {
+    setSearchTerm(searchTerm);
+  };
+
+  const handlePageChange = (newPage: number) => {
+    // Implement your pagination logic here
+    console.log(newPage);
+    console.log(searchTerm);
+  };
 
   return (
-    <main className={"flex flex-col items-center"}>
-      <Header></Header>
-      <div
-        className={
-          " max-w-2xl w-full w-xl flex justify-center items-center my-4"
-        }
-      >
-        <SearchBar onSearch={HandleSearch}></SearchBar>
+    <main className="flex flex-col items-center gap-24">
+      <Header />
+      <div className="mt-24 max-w-2xl w-full flex justify-center items-center my-4">
+        <SearchBar onSearch={handleSearch} />
       </div>
-      {places && <PlacesCard data={places[0]}></PlacesCard>}
-      {!places && <h1>No Items!</h1>}
+      {places.length > 0 ? <PlacesList places={places} /> : <h1>No Items!</h1>}
       <Pagination
-        totalPages={places ? places.length : 0}
+        totalPages={places.length}
         currentPage={0}
-        onPageChange={() => {}}
-      ></Pagination>
-      <Footer bg="gray-100"></Footer>
+        onPageChange={handlePageChange}
+      />
+      <Footer bg="gray-100" />
     </main>
   );
 };
