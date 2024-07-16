f833afd1f9ea2d624cda766443e9fc7e8564a650
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-16 22:30:35 +0300

    feat(API): update placesData to include importand data

--- frontend/src/API/places.ts
+++ frontend/src/API/places.ts
@@ -1,17 +1,45 @@
+import boat from "../assets/boat+mountain.jpg";
+
 export interface FilterMap {
   [field: string]: string;
 }
 
 export interface PlaceData {
   // Until the data needed is required
-  name: string;
+  location: string;
+  image: string;
+  hasHiking: boolean;
+  hasSkiing: boolean;
+  hasFestival: boolean;
+  hasMuseum: boolean;
+  hasZoo: boolean;
 }
 
 export function getPlaces(filters: FilterMap[] | null = []): PlaceData[] {
   // Example return data matching the expected type
   if (filters != null) {
-    return [{ name: "Harar" }];
+    return [
+      {
+        location: "Harar",
+        image: boat,
+        hasHiking: true,
+        hasSkiing: true,
+        hasMuseum: true,
+        hasFestival: true,
+        hasZoo: true,
+      },
+    ];
   }
 
-  return [{ name: "Harar" }];
+  return [
+    {
+      location: "Harar",
+      image: boat,
+      hasHiking: true,
+      hasSkiing: true,
+      hasMuseum: true,
+      hasFestival: true,
+      hasZoo: true,
+    },
+  ];
 }
