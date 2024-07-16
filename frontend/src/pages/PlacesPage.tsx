126ce4831e365a9966b90734def4dc328019f643
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-16 23:55:13 +0300

    feat(pages): implement pagination in places page

--- frontend/src/pages/PlacesPage.tsx
+++ frontend/src/pages/PlacesPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getPlaces, PlaceData } from "../API/places";
+import { FilterMap, getPlaces, PlaceData } from "../API/places";
 import "../styles/output.css";
 import SearchBar from "../components/core/SearchBar";
 import Pagination from "../components/core/PaginationComponent";
@@ -10,28 +10,34 @@ import PlacesList from "../components/Places/PlacesList";
 const PlacesPages = () => {
   const [places, setPlaces] = useState<PlaceData[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [filters, setFilters] = useState<FilterMap>({});
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 10;
 
   useEffect(() => {
     const fetchPlaces = async () => {
       try {
-        const fetchedPlaces = await getPlaces();
+        const fetchedPlaces = await getPlaces(filters, searchTerm);
         setPlaces(fetchedPlaces);
       } catch (error) {
         console.log("Error fetching places:", error);
       }
     };
     fetchPlaces();
-  }, []);
+  }, [searchTerm, filters]);
 
   const handleSearch = (searchTerm: string) => {
     setSearchTerm(searchTerm);
+    setCurrentPage(1); // Reset to first page on search
   };
 
   const handlePageChange = (newPage: number) => {
-    // Implement your pagination logic here
-    console.log(newPage);
-    console.log(searchTerm);
+    setCurrentPage(newPage);
   };
+
+  const indexOfLastPlace = currentPage * itemsPerPage;
+  const indexOfFirstPlace = indexOfLastPlace - itemsPerPage;
+  const currentPlaces = places.slice(indexOfFirstPlace, indexOfLastPlace);
 
   return (
     <main className="flex flex-col items-center gap-24">
@@ -39,10 +45,14 @@ const PlacesPages = () => {
       <div className="mt-24 max-w-2xl w-full flex justify-center items-center my-4">
         <SearchBar onSearch={handleSearch} />
       </div>
-      {places.length > 0 ? <PlacesList places={places} /> : <h1>No Items!</h1>}
+      {places.length > 0 ? (
+        <PlacesList places={currentPlaces} />
+      ) : (
+        <h1>No Items!</h1>
+      )}
       <Pagination
-        totalPages={places.length}
-        currentPage={0}
+        totalPages={Math.ceil(places.length / itemsPerPage)}
+        currentPage={currentPage}
         onPageChange={handlePageChange}
       />
       <Footer bg="gray-100" />
