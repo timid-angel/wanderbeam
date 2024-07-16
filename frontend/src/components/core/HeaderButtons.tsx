b161db953f36459cc4f88db319d66ddadfe743ff
Nathan M.S <natms24@gmail.com>
2024-07-16 16:12:58 +0300

    Add language dropdown and block overscrolling

--- frontend/src/components/core/HeaderButtons.tsx
+++ frontend/src/components/core/HeaderButtons.tsx
@@ -1,5 +1,6 @@
-import { styled } from "@mui/material";
+import { FormControl, MenuItem, Select, styled } from "@mui/material";
 import Button from "@mui/material/Button";
+import ReactCountryFlag from "react-country-flag";
 
 const StyledButton = styled(Button)(`
   font-weight: bold;
@@ -17,7 +18,27 @@ const HeaderButtons = () => {
         Login
       </StyledButton>
       <Button variant="contained">Signup</Button>
-      <Button variant="outlined">EN</Button>
+      <div className="h-fit">
+        <FormControl sx={{ m: 1 }} size="small">
+          <Select
+            inputProps={{ "aria-label": "Without label" }}
+            defaultValue="en"
+          >
+            <MenuItem value="en">
+              <div className="flex justify-center items-center gap-2">
+                <p className="text-blue-800">EN</p>
+                <ReactCountryFlag countryCode="GB" />
+              </div>
+            </MenuItem>
+            <MenuItem value="am">
+              <div className="flex justify-center items-center gap-2">
+                <p className="text-blue-800">AM</p>
+                <ReactCountryFlag countryCode="ET" />
+              </div>
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 };
