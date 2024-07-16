95e4493556d3f4569827cf556d88e83e7f292d3c
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-16 16:40:51 +0300

    feat(pages): add search bar and pagination to the places page

--- frontend/src/pages/PlacesPage.tsx
+++ frontend/src/pages/PlacesPage.tsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import { getPlaces, PlaceData } from "../API/places";
+import "../styles/output.css";
+import SearchBar from "../components/common/SearchBar";
+import { Pagination } from "@mui/material";
+import PlacesCard from "../components/Places/PlacesCard";
+
 function PlacesPages() {
-  return <h1></h1>;
+  const [places, setPlaces] = useState<PlaceData[] | null>(null);
+
+  useEffect(() => {
+    let fetchedPlaces = null;
+    try {
+      fetchedPlaces = getPlaces();
+    } catch {
+      console.log("error");
+    }
+    setPlaces(fetchedPlaces);
+  }, []);
+
+  function HandleSearch(searchTerm: string): void {
+    if (!searchTerm) {
+      throw new Error("Function not implemented.");
+    }
+  }
+
+  return (
+    <main className={"flex flex-col items-center"}>
+      <div
+        className={
+          "max-w-2xl w-full w-xl flex justify-center items-center my-4"
+        }
+      >
+        <SearchBar onSearch={HandleSearch}></SearchBar>
+      </div>
+      {places && <PlacesCard data={places[0]}></PlacesCard>}
+      {!places && <h1>No Items!</h1>}
+      <Pagination></Pagination>
+    </main>
+  );
 }
 
 export default PlacesPages;
