fc4693f443bb61646877905c52d29eaa28250e64
zemenu <zemenumekiria@gmail.com>
2024-07-16 22:19:39 +0300

    activities page completed

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LodgingPage from "./pages/LodgingPage";
+import ActivitiesPage from "./pages/ActivitiesPage"
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/lodging",
     element: <LodgingPage/>
+  },
+  {
+    path:"/activities",
+    element:<ActivitiesPage/>
   }
 ]);
