7ef110019751fc9b1ddc67df61f031f44b7a5846
ephrem <ephremmamo555@gmail.com>
2024-07-16 13:01:20 +0300

    added pagination component

---
+++ frontend/src/components/PaginationComponent.tsx
@@ -0,0 +1,36 @@
+import './Pagination.css'
+import React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+
+interface PaginationComponentProps {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+const PaginationComponent: React.FC<PaginationComponentProps> = ({
+  totalPages,
+  currentPage,
+  onPageChange,
+}) => {
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    onPageChange(value);
+  };
+
+  return (
+    <div className="pagination-container">
+      <Stack spacing={2}>
+        <Pagination
+          count={totalPages}
+          page={currentPage}
+          onChange={handleChange}
+          variant="outlined"
+          shape="rounded"
+        />
+      </Stack>
+    </div>
+  );
+};
+
+export default PaginationComponent;
