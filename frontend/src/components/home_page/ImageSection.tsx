6277c3503fc62f9103934fa9ac08dec018812412
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-07-17 11:05:31 +0300

    fix(pages): fix navigaion to places in home page

--- frontend/src/components/home_page/ImageSection.tsx
+++ frontend/src/components/home_page/ImageSection.tsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import moutains from "../../assets/mountains.jpg";
 
 const ImageSection = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate("/places");
+  };
+
   return (
     <div className="max-h-screen w-full">
       <img
@@ -12,7 +19,10 @@ const ImageSection = () => {
         <p className="text-[64px] italic font-semibold text-slate-50">
           Plan your travels
         </p>
-        <div className="rounded-full px-8 py-3 bg-[#0057FF] text-[20px] hover:bg-[#0044ff] font-semibold text-slate-50 w-fit">
+        <div
+          onClick={handleNavigation}
+          className=" cursor-pointer rounded-full px-8 py-3 bg-[#0057FF] text-[20px] hover:bg-[#0044ff] font-semibold text-slate-50 w-fit"
+        >
           Build Itinerary
         </div>
       </div>
